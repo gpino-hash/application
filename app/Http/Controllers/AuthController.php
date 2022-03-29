3324baa67e23eaf85e2ab7e0578f757c2390d902
Gustavo Pino <gustavopino@hotmail.es>
2022-03-29 09:10:09 -0300

    change response

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -6,46 +6,52 @@ use App\Http\Factory\Auth\GuardName;
 use App\Http\Factory\Auth\Impl\ApiAuthentication;
 use App\Http\Factory\Auth\Impl\SocialNetworkAuthentication;
 use App\Http\Requests\AuthRequest;
+use App\Http\Traits\ResponseWithHttpStatus;
 use App\Http\UseCase\TypeSocialNetworks;
 use Illuminate\Auth\AuthenticationException;
-use Illuminate\Http\JsonResponse;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Response;
 use Laravel\Socialite\Facades\Socialite;
+use Throwable;
 
 class AuthController extends Controller
 {
+    use ResponseWithHttpStatus;
+
     /**
      * @param AuthRequest $request
-     * @return JsonResponse
+     * @return Application|ResponseFactory|Response
      */
-    public function login(AuthRequest $request): JsonResponse
+    public function login(AuthRequest $request): Application|ResponseFactory|Response
     {
         try {
             $api = new ApiAuthentication(GuardName::WEB,
                 $request->only("email", "password"),
                 $request->boolean("remember"));
-            return response()->json($api->handle());
+            return $this->success("Request made successfully.", $api->handle());
         } catch (AuthenticationException $authenticationException) {
-            return response()->json("error al autenticarse");
-        } catch (\Throwable $exception) {
-            return response()->json("error al interno del servidor");
+            return $this->failure("Failed to authenticate. User or password is wrong.");
+        } catch (Throwable $exception) {
+            return $this->failure("We are currently having difficulties, please try again later.", Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 
     /**
      * @param string $socialNetwork
-     * @return JsonResponse
+     * @return Application|Response|ResponseFactory
      */
-    public function loginWithSocialNetwork(string $socialNetwork): JsonResponse
+    public function loginWithSocialNetwork(string $socialNetwork): Application|ResponseFactory|Response
     {
         try {
             $social = new SocialNetworkAuthentication(GuardName::WEB,
                 TypeSocialNetworks::getTypeSocialNetworks($socialNetwork));
-            return response()->json($social->handle());
+            return $this->success("Request made successfully.", $social->handle());
         } catch (AuthenticationException $authenticationException) {
-            return response()->json("error al autenticarse");
-        } catch (\Throwable $exception) {
-            return response()->json("error al interno del servidor");
+            return $this->failure("Failed to authenticate. User or password is wrong.");
+        } catch (Throwable $exception) {
+            return $this->failure("We are currently having difficulties, please try again later.", Response::HTTP_INTERNAL_SERVER_ERROR);
         }
     }
 
---
+++ app/Http/Traits/ResponseWithHttpStatus.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Traits;
+
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Http\Response;
+
+trait ResponseWithHttpStatus
+{
+    /**
+     * @param $message
+     * @param array $data
+     * @param int $status
+     * @return Application|ResponseFactory|Response
+     */
+    protected function success($message, array $data = [], int $status = 200): Response|Application|ResponseFactory
+    {
+        return response(['success' => true, 'message' => $message, 'data' => $data,], $status);
+    }
+
+    /**
+     * @param $message
+     * @param int $status
+     * @return Application|ResponseFactory|Response
+     */
+    protected function failure($message, int $status = 422): Response|Application|ResponseFactory
+    {
+        return response(['success' => false, 'message' => $message,], $status);
+    }
+}
