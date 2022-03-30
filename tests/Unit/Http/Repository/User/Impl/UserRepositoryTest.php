03b4beeaeaf8bbf7129fccfd526306ebb30ad06a
Gustavo Pino <gustavopino@hotmail.es>
2022-03-30 15:27:08 -0300

    add tests

---
+++ tests/Unit/Http/Repository/User/Impl/UserRepositoryTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Tests\Unit\Http\Repository\User\Impl;
+
+use App\Http\Repository\User\Impl\UserRepository;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Builder;
+use Tests\TestCase;
+
+class UserRepositoryTest extends TestCase
+{
+    /**
+     * @testdox
+     * @test
+     */
+    public function caseOne()
+    {
+        $repository = new UserRepository();
+        $this->assertNull($repository->getUserByEmail("test"));
+    }
+
+    /**
+     * @testdox
+     * @test
+     */
+    public function caseTwo()
+    {
+        $user = User::factory()->make();
+
+        $builder = $this->mock(Builder::class);
+        $builder->shouldReceive("where")->with("email", $user->email)->andReturnSelf();
+        $builder->shouldReceive('first')->andReturn($user);
+
+        $mockUser = $this->mock(User::class);
+        $mockUser->shouldReceive('query')->andReturn($builder);
+
+        $repository = new UserRepository();
+        $this->assertEquals($user, $repository->getUserByEmail($user->email));
+    }
+}
